59eb4b87d68f7ad8c71aa8f0374d07d0cacd8d65
Anilbal <anilbal7676@gmail.com>
2024-08-20 00:40:34 +0545

    variables in  ts

---
+++ 01_intro/varibales.ts
@@ -0,0 +1,18 @@
+//number 
+let num:number=33222
+
+// boolean
+let isLoggedIn:boolean=true
+
+// string
+let myName:string="Hello Typescript"
+
+
+//any 
+//let hero;     here hero is not specifing what value is going to be assigned so it return any keywords
+let hero:string; //here it specific that hero should only be strings 
+
+function getHero(){
+    return "hello"
+}
+hero=getHero()
